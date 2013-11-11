a37e7688978b16339ec37001561734235e70f6ec
Tom <tom@tomleo.com>
2013-11-11 10:35:02 -0500

    Update __init__.py
    Looks like its looking in `menu_module = '%s.menus' % app` not menu.py

--- menu/__init__.py
+++ menu/__init__.py
@@ -16,7 +16,7 @@ class Menu(object):
     using the generate_menu template tag.
 
     Menus are loaded from the INSTALLED_APPS, inside a file named
-    menu.py. This file should import the Menu & MenuItem classes and
+    menus.py. This file should import the Menu & MenuItem classes and
     then call add_item:
 
     Menu.add_item("main", MenuItem("My item", reverse("myapp.views.myview"), weight=10))
