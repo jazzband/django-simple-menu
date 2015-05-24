b374d3dd801a5d0746ce28e8c182a64ce05917d7
Evan Borgstrom <eborgstrom@linkedin.com>
2015-05-24 15:22:42 -0700

    Version 1.1.0 - Transfer ownership to Evan Borgstrom

--- menu/__init__.py
+++ menu/__init__.py
@@ -1,4 +1,4 @@
 from .menu import Menu, MenuItem
 
-__version__ = '1.0.9'
-__url__ = 'https://github.com/fatbox/django-simple-menu'
+__version__ = '1.1.0'
+__url__ = 'https://github.com/borgstrom/django-simple-menu'
