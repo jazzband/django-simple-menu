0de7f1ad01ce092ecfc9196a9776c61d149d4bf5
Nikita Karamov <me@kytta.dev>
2022-05-31 20:23:02 +0200

    Delete old example app

--- example/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import *
-
-urlpatterns = patterns(
-    'accounts.views',
-
-    url(r'profile/', 'profile'),
-    url(r'', 'index')
-)
--- example/reports/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import *
-
-urlpatterns = patterns(
-    'reports.views',
-
-    url(r'staff/', 'staff'),
-    url(r'superuser', 'superuser'),
-    url(r'', 'index'),
-)
