b68a78ca13c5af06e5071454198a1239ac49b6c5
Nikita Karamov <me@kytta.dev>
2022-06-06 14:41:47 +0200

    Allow importing 'menu' with warning

---
+++ menu/__init__.py
@@ -0,0 +1,9 @@
+import warnings
+
+from menu import *
+
+warnings.warn(
+	"Importing from 'menu' will be removed in django-simple-menu v2.3 and/or "
+	"v3.0. Use imports from 'simple_menu' package instead.",
+	DeprecationWarning
+)
