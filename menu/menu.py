dc4d66ae400efd11c48fc28b40881602b40c2b2e
Nikita Karamov <me@kytta.dev>
2022-08-26 22:15:22 +0200

    Replace tabs with spaces

--- menu/menu.py
+++ menu/menu.py
@@ -3,8 +3,8 @@ import warnings
 from simple_menu.menu import *
 
 warnings.warn(
-	"Importing from 'menu' will be removed in django-simple-menu v2.3 and/or "
-	"v3.0. Use imports from 'simple_menu' package instead.",
-	DeprecationWarning,
-	stacklevel=2
+    "Importing from 'menu' will be removed in django-simple-menu v2.3 and/or "
+    "v3.0. Use imports from 'simple_menu' package instead.",
+    DeprecationWarning,
+    stacklevel=2
 )
