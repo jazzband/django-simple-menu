accc53272c82666fe4022dbd3edce5aba78b7985
Nikita Karamov <me@kytta.dev>
2022-08-27 00:27:00 +0200

    Use importlib_metadata to retrieve version
    This small dependency allows us to reduce latency when importing. It can
    be removed when we drop Python 3.7 support.
    
    See also:
    https://github.com/pypa/setuptools_scm/#retrieving-package-version-at-runtime

--- simple_menu/__init__.py
+++ simple_menu/__init__.py
@@ -1,9 +1,10 @@
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib_metadata import PackageNotFoundError, version
+
 from .menu import Menu, MenuItem
 
 try:
-    __version__ = get_distribution("django-simple-menu").version
-except DistributionNotFound:
+    __version__ = version("django-simple-menu")
+except PackageNotFoundError:
     # package is not installed
     __version__ = None
 
