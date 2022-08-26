b0c6a63134249a9b8929da20ca657269783ffa9f
Nikita Karamov <me@kytta.dev>
2022-08-26 23:43:39 +0200

    Enable MENU_SELECT_PARENTS

--- example/example/settings.py
+++ example/example/settings.py
@@ -119,3 +119,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MENU_SELECT_PARENTS = True
