108011a7022927a5fd34e74471f4bd840ffe6d1e
Nikita Karamov <me@kytta.dev>
2025-05-05 23:20:05 +0200

    Move warning from simple_menu to menu

--- menu/templatetags/menu.py
+++ menu/templatetags/menu.py
@@ -1,1 +1,10 @@
-from simple_menu.templatetags.menu import *
+import warnings
+
+from simple_menu.templatetags.simple_menu import *
+
+warnings.warn(
+    "The 'menu' template library will be removed in django-simple-menu v2.3 "
+    "and/or v3.0. Use '{% load simple_menu %}' instead.",
+    DeprecationWarning,
+    stacklevel=2
+)
